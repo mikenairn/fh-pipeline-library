a44ffcdf0a979e9f0c7b920df6e0cd815e923338
Michael Nairn <mnairn@redhat.com>
2017-05-29 15:19:53 +0100

    Add buildInfo

--- vars/gruntBuild.groovy
+++ vars/gruntBuild.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/groovy
 
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurperClassic
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
@@ -13,5 +16,20 @@ def call(body) {
         cmd = distCmd
     }
 
-    archiveArtifacts "dist/${name}*.tar.gz, output/**/VERSION.txt"
+    def versionTxt = readFile "output/**/VERSION.txt"
+
+    def buildInfoFileName = 'build-info.json'
+    def buildInfo = fileExists(buildInfoFileName) ? new JsonSlurperClassic().parseText(readFile(buildInfoFileName)) : [:]
+
+    def (version,build) = versionTxt.trim().split('-')
+    println version
+    println build
+
+    buildInfo[name] = [:]
+    buildInfo[name]['version'] = versionTxt[0]
+    buildInfo[name]['build'] = env.BUILD_NUMBER
+
+    writeFile file: buildInfoFileName, text: new JsonBuilder(buildInfo).toPrettyString()
+
+    archiveArtifacts "dist/${name}*.tar.gz, ${buildInfoFileName}"
 }
