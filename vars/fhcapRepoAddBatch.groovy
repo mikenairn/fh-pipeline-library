2c4f9c327a872c4e1f0765bdcd90b89c321683b9
Michael Nairn <mnairn@redhat.com>
2017-06-27 14:03:31 +0100

    Update fhcapRepoAddBatch to allow passing in repo optional repo config.

--- vars/fhcapRepoAddBatch.groovy
+++ vars/fhcapRepoAddBatch.groovy
@@ -12,9 +12,21 @@ def call(body) {
     def repos = config.repos ?: [:]
 
     for (def repo in utils.mapToList(repos)) {
+        def repoName = repo[0]
+        def repoUrl = null
+        def repoClustersDir = null
+        if(repo[1] instanceof Map) {
+            repoUrl = repo[1]['url']
+            repoClustersDir = repo[1]['clustersDir']
+        } else {
+            repoUrl = repo[1]
+            repoClustersDir = 'clusters'
+        }
         fhcapRepoAdd {
-            name = repo[0]
-            url = repo[1]
+            name = repoName
+            url = repoUrl
+            clustersDir = repoClustersDir
         }
     }
+
 }
